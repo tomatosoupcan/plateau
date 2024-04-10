22ba916c500f91fefafef5f702534478fad364b7
tomatosoupcan <benjamin.chuba@gmail.com>
2024-04-09 20:40:37 -0400

    fix clear board

--- index.js
+++ index.js
@@ -157,8 +157,12 @@ function clearBoard() {
     id2 = 'i' + i;
     document.getElementById(id2).style.display = 'block';        
     document.getElementById(id1).classList.add("bgwhite");
-    document.getElementById(id1).classList.remove("bggold");
-    document.getElementById(id1).classList.remove("bgblack");
+	document.getElementById(id).classList.remove("bgblack");
+	document.getElementById(id).classList.remove("bgred");
+	document.getElementById(id).classList.remove("bgblue");
+	document.getElementById(id).classList.remove("bgpurple");
+	document.getElementById(id).classList.remove("bggreen");
+	document.getElementById(id).classList.remove("bggold");
   }
 }
 
